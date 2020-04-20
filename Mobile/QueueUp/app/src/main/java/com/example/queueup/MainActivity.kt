41a1445f8e1ea1be19ae6f26c5fb8d79038a9ec4
Raissa Raide <500764@soa.ad.pwcinternal.com>
2020-04-20 19:02:44 -0300

    Adding feature for user login and register.

--- Mobile/QueueUp/app/src/main/java/com/example/queueup/MainActivity.kt
+++ Mobile/QueueUp/app/src/main/java/com/example/queueup/MainActivity.kt
@@ -3,16 +3,45 @@ package com.example.queueup
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
 import android.view.View
+import android.widget.ImageView
+import androidx.appcompat.app.AlertDialog
+import kotlinx.android.synthetic.main.activity_main.*
 
- class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        //button click to show modal
+        btnCadastrar.setOnClickListener() {
+            val mDialogView = LayoutInflater.from(this).inflate(R.layout.modal_cadastro, null);
+            val mBuilder = AlertDialog.Builder(this)
+                .setView(mDialogView)
+            //show dialog
+            val mAlertDialog = mBuilder.show()
+            //close modal
+            val close: ImageView = mDialogView.findViewById(R.id.imgExit)
+            close.setOnClickListener{
+                mAlertDialog.dismiss()
+            }
+        }
     }
+
     fun irTelaMaps(v: View){
-        val tlMaps = Intent(this, MapsActivity::class.java)
+       val tlMaps = Intent(this, MapsActivity::class.java)
         startActivity(tlMaps)
     }
+
+     //test functionality
+     fun irTelaRestaurante(v: View){
+         val telaRestaurante = Intent(this, MainRestaurante::class.java)
+         startActivity(telaRestaurante)
+     }
+     fun irCadastroUsuario(v: View){
+         val cadastrousuario = Intent(this, Cadastro_usuario::class.java)
+         startActivity(cadastrousuario)
+     }
+
 }
