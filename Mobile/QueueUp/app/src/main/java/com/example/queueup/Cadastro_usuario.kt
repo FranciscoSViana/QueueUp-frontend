92beef2718084bc8511c2f9fb654c9f7ae89588e
FrankSVin <franciscosantos29@gmail.com>
2020-04-27 21:02:36 -0300

    front encaminhado

--- Mobile/QueueUp/app/src/main/java/com/example/queueup/Cadastro_usuario.kt
+++ Mobile/QueueUp/app/src/main/java/com/example/queueup/Cadastro_usuario.kt
@@ -10,42 +10,17 @@ import kotlinx.android.synthetic.main.activity_main.*
 
 class Cadastro_usuario : AppCompatActivity() {
 
-    var preferencias: SharedPreferences? = null
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_cadastro_usuario)
 
-        preferencias = getPreferences(Context.MODE_PRIVATE)
 
-        etUsuario.setText(preferencias?.getString("loginUsuario", ""))
-
-        fun salvarDados(v: View) {
-
-            val editor = preferencias?.edit()
-
-            editor?.putString("loginUsuario", etUsuario.text.toString())
-
-            editor?.commit()
-        }
-
-        fun limparDados(v: View) {
-            val editor = preferencias?.edit()
-
-            editor?.clear()
-            editor?.commit()
-        }
     }
 
     fun registerUser(v: View) {
         val register = Intent(this, MainActivity::class.java)
         startActivity(register)
 
-        val editor = preferencias?.edit()
-
-        editor?.putString("loginUsuario", etUsuario.text.toString())
-
-        editor?.commit()
     }
 }
